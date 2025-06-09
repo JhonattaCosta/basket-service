7dde2ac6e361024804c52c990d09d41159456888
JhonattaCosta <jhonattasantos27@gmail.com>
2025-06-09 19:27:45 -0300

    feat; add Redis cache config

--- src/main/java/dev/java/ecommerce/basketservice/service/ProductService.java
+++ src/main/java/dev/java/ecommerce/basketservice/service/ProductService.java
@@ -5,21 +5,27 @@ import dev.java.ecommerce.basketservice.client.PlatziStoreClient;
 
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ProductService {
 
     private final PlatziStoreClient platziStoreClient;
 
-
+    @Cacheable(value = "products")
     public List<PlatziProductResponse> getAllProducts(){
+        log.info("Getting all Products");
         return platziStoreClient.getAllProducts();
     }
+    @Cacheable(value = "products", key = "#id")
     public PlatziProductResponse getProductId(Long id){
+        log.info("Getting product with id: {}", id);
         return platziStoreClient.getProductId(id);
     }
 
